18df7a66d0d15baf3aacf69da5bdd5ef6330fe2e
AliAzaz <ali.azaz.alam@hotmail.com>
2020-12-17 19:19:40 +0500

    *Working on childlist

---
+++ app/src/main/java/edu/aku/hassannaqvi/matiari_cohorts/ui/sections/dashboardActivity/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package edu.aku.hassannaqvi.matiari_cohorts.ui.sections.dashboardActivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.aku.hassannaqvi.matiari_cohorts.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
